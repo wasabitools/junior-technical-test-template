4932f8ab03e88ca2c1c216667b1ed6ceda3057ac
natasafrgk <natasa.fragkou@airelogic.com>
2024-12-09 16:57:35 +0000

    basic /event implementation

--- user_monitoring/api.py
+++ user_monitoring/api.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, current_app
+"""
+Implements /event endpoint
+"""
+from flask import Blueprint, current_app, request
 
 
 api = Blueprint("api", __name__)
@@ -6,5 +9,19 @@ api = Blueprint("api", __name__)
 
 @api.post("/event")
 def handle_user_event() -> dict:
+    "Handles user event"
     current_app.logger.info("Handling user event")
-    return {}
+
+    req = request.get_json()
+
+    user_id = req["user_id"]
+    event_type = req["type"]
+    amount = req["amount"]
+    timestamp = req["time"]
+
+    return {
+        "event_type": event_type,
+        "amount": amount,
+        "user_id": user_id,
+        "timestamp": timestamp,
+    }
