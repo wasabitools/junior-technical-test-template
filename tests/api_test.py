c4be87c591c13807653396b125c8df0f5cf8f024
natasafrgk <natasa.fragkou@airelogic.com>
2024-12-10 14:57:33 +0000

    tests 1100 code

--- tests/api_test.py
+++ tests/api_test.py
@@ -5,12 +5,17 @@ def test_handle_user_event_success(client: FlaskClient) -> None:
     """
     Tests the successful handling of a user event.
     """
-    payload = {
-        "type": "deposit",
-        "amount": "10.00",
-        "user_id": 1,
-        "time": 0
-    }
+    payload = {"type": "deposit", "amount": "10.00", "user_id": 1, "time": 0}
     response = client.post("/event", json=payload)
     assert response.status_code == 200
     assert response.json == {"alert": False, "alert_codes": [], "user_id": 1}
+
+
+def test_handle_large_withdrawal(client: FlaskClient) -> None:
+    """
+    Tests triggering the 1100 alert for a withdrawal over 100.
+    """
+    payload = {"type": "withdraw", "amount": "120.00", "user_id": 1, "time": 10}
+    response = client.post("/event", json=payload)
+    assert response.status_code == 200
+    assert response.json == {"alert": True, "alert_codes": [1100], "user_id": 1}
